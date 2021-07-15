9c434094d8c43591a4be46716ebd65e77f46f127
Garrett McGrath <gmcgrath815@gmail.com>
2021-07-15 13:16:29 -0400

    this is a minimum viable version of the package, currently managing the agent 1 variant. considering moving to agent 2

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,10 @@
 class pnizabbix::params{
 
 $server=lookup('pnizabbix::server')
+#$server=hiera('pnizabbix::server')
 
 ## by default just listen on all ports - note: this can cause problems if it detects ipv6 capabilities
 $agentlistenip=lookup('pnizabbix::agent::listenip',undef,undef,'0.0.0.0')
+#$agentlistenip=hiera('pnizabbix::agent::listenip','0.0.0.0')
 
 }
